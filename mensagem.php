5f301a1bb4a59fb990938cbadea2916ad130c1f7
danonezz <dionislessa@gmail.com>
2022-04-29 20:09:50 -0300

    primeiro commit

---
+++ mensagem.php
@@ -0,0 +1,56 @@
+<html>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style>
+    @font-face {
+        font-family: Kanit;
+        src: url(https://fonts.googleapis.com/css2?family=Kanit&display=swap);
+    }
+
+    p {
+        font-family: Kanit;
+        font-size: 22px;
+        color: 090c0b;
+    }
+
+    #mensagem {
+        opacity: 0%;
+        left: 10px;
+        width: 300px;
+        border-radius: 8px;
+        padding: 1px 25px 1px 25px;
+        text-align: center;
+        position: absolute;
+        height: fit-content;
+        word-wrap: break-word;
+        background-color: d5cdc8;
+        animation-name: fadein;
+        animation-duration: .5s;
+        animation-timing-function: ease-in-out;
+        animation-fill-mode: forwards;
+    }
+
+    @keyframes fadein {
+        0% {
+            top: 150px;
+        }
+
+        100% {
+            top: 250px;
+            opacity: 100%;
+        }
+    }
+</style>
+
+<body onclick="document.getElementById('video').play();">
+    <?php
+    $myfile = fopen("D:\Google Drive\Stream\dns\Stream 3.0\Interações\mensagem\html\mensagem.txt", "r");
+    $message = fread($myfile, filesize("D:\Google Drive\Stream\dns\Stream 3.0\Interações\mensagem\html\mensagem.txt"));
+    fclose($myfile);
+    ?>
+    </source>
+    <div id="mensagem">
+        <p><?php echo $message; ?></p>
+    </div>
+</body>
+
+</html>
